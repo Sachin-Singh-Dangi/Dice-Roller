cb332998527324b565a0428e9749bbc050d8281b
Sachin Singh Dangi <112658166+Sachin-Singh-Dangi@users.noreply.github.com>
2022-11-28 14:24:50 +0530

    Update myscript.js

--- js/myscript.js
+++ js/myscript.js
@@ -1,8 +1,8 @@
 // function to roll dice
 function diceRoll() {
   var n = Math.floor(Math.random() * 6 + 1);
-  document.querySelector(".num").innerText = n;
   document
     .querySelector(".dice")
     .setAttribute("src", "images/dice" + n + ".png");
+  document.querySelector(".num").innerText = n;
 }
